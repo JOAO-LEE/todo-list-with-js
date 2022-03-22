f4ad3d857bd6435713380497d5974a4edabdcc4a
João Lee <joaumlimaum@gmail.com>
2022-03-22 18:59:17 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-//1 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
-// const myBody = document.getElementsByTagName("body")[0];
-// function headerCreation() {
-//     let myHeader = document.createElement("header");
-//     myHeader.id = "top-info";
-//     myHeader.innerHTML = "Minha Lista de Tarefas"
-//     myBody.appendChild(myHeader);
-// }
-// headerCreation();
+const myButton = document.querySelector('button');
+const myList = document.querySelector('ol');
+const writingBlock = document.querySelector('input');
 
-// // const headingText = document.getElementsByTagName("header")[0];
-// // function creatingTitle(){
-// //     let createTitle = document.createElement("h1");
-// //     createTitle.innerText = "Minha Lista de Tarefas";
-// //     createTitle.id = "head-title";
-// //     headingText.appendChild(createTitle);
-// // }
-// // creatingTitle();
+function insertTasks() {
+    let lines = document.createElement('li');
+    lines.innerText = writingBlock.value;
+    myList.appendChild(lines);
+    writingBlock.value = '';
+}
+myButton.addEventListener('click', insertTasks)
