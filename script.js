5023a3a33c5320371dea77f82e1f87326d84dbde
João Lee <joaumlimaum@gmail.com>
2022-03-22 17:58:01 -0300

    Correçao Requisito 1, envio Requisito 2

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 //1 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
-const myBody = document.getElementsByTagName("body")[0];
-function headerCreation() {
-    let myHeader = document.createElement("header");
-    myHeader.id = "top-info";
-    myHeader.innerHTML = "Minha Lista de Tarefas"
-    myBody.appendChild(myHeader);
-}
-headerCreation();
-
-// const headingText = document.getElementsByTagName("header")[0];
-// function creatingTitle(){
-//     let createTitle = document.createElement("h1");
-//     createTitle.innerText = "Minha Lista de Tarefas";
-//     createTitle.id = "head-title";
-//     headingText.appendChild(createTitle);
+// const myBody = document.getElementsByTagName("body")[0];
+// function headerCreation() {
+//     let myHeader = document.createElement("header");
+//     myHeader.id = "top-info";
+//     myHeader.innerHTML = "Minha Lista de Tarefas"
+//     myBody.appendChild(myHeader);
 // }
-// creatingTitle();
+// headerCreation();
+
+// // const headingText = document.getElementsByTagName("header")[0];
+// // function creatingTitle(){
+// //     let createTitle = document.createElement("h1");
+// //     createTitle.innerText = "Minha Lista de Tarefas";
+// //     createTitle.id = "head-title";
+// //     headingText.appendChild(createTitle);
+// // }
+// // creatingTitle();
