aefc1b9d047222718e5d0f80f4d9af83aa9b63ed
João Lee <joaumlimaum@gmail.com>
2022-03-23 23:28:11 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -12,16 +12,19 @@ function insertTasks() {
 }
 addButton.addEventListener('click', insertTasks)
 // 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza
-// 8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo
 const listItemsLocation = document.getElementById('lista-tarefas');
 function changeBackgroundItems(event) {
   event.target.style.background = 'grey';
-  
 }
 listItemsLocation.addEventListener('click', changeBackgroundItems)
+// 8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo
 
-// function removeListItems
+// 9 - Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item
+function triscaAi(addClass){
+    addClass.target.classList.toggle("completed")
 
+}
+listItemsLocation.addEventListener('dblclick', triscaAi)
 // 10 - Adicione um botão com id="apaga-tudo" que quando clicado deve apagar todos os itens da lista
 let items = document.querySelectorAll('.item')
 const removeButton = document.querySelector('#apaga-tudo')
@@ -29,4 +32,3 @@ removeButton.addEventListener('click', liRemover)
 function liRemover() {
     myList.innerHTML = '';
 }
-
