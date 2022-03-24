a348d3cd0160e9640133b0bdcbec9294e7d4a5f0
João Lee <joaumlimaum@gmail.com>
2022-03-23 21:15:45 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const myButton = document.querySelector('#criar-tarefa');
+const addButton = document.querySelector('#criar-tarefa');
 const myList = document.querySelector('ol');
 const writingBlock = document.querySelector('input');
+
 // 6 - Ordene os itens da lista de tarefas por ordem de criação
 function insertTasks() {
     let lines = document.createElement('li');
@@ -9,11 +10,23 @@ function insertTasks() {
     myList.appendChild(lines);
     writingBlock.value = '';
 }
-myButton.addEventListener('click', insertTasks)
-let listItems = document.querySelectorAll('.item')
+addButton.addEventListener('click', insertTasks)
 // 7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza
-const listItemLocation = document.getElementById('lista-tarefas');
-function changeBackground(event) {
+// 8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo
+const listItemsLocation = document.getElementById('lista-tarefas');
+function changeBackgroundItems(event) {
   event.target.style.background = 'grey';
+  
 }
-listItemLocation.addEventListener('click', changeBackground)
+listItemsLocation.addEventListener('click', changeBackgroundItems)
+
+// function removeListItems
+
+// 10 - Adicione um botão com id="apaga-tudo" que quando clicado deve apagar todos os itens da lista
+let items = document.querySelectorAll('.item')
+const removeButton = document.querySelector('#apaga-tudo')
+removeButton.addEventListener('click', liRemover)
+function liRemover() {
+    myList.innerHTML = '';
+}
+
